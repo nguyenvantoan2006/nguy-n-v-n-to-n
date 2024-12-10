5879aa6e11eddfd2e3c533a508ca801319927a12
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-10 06:39:56 -0800

    Add files via upload

---
+++ baitap6.cpp
@@ -0,0 +1,10 @@
+#include<stido.h>
+int main(){
+    float Pi=3,14, R=5, chuVi,dienTich;
+    chuVi = 2*Pi*R ;
+    printf("chuVi hinh tron la %2f\n",chuVi);
+    dienTich =R*R*Pi;
+    printf("S hinh tron la %2f\n",dienTich);
+    Return 0;
+}
+    
