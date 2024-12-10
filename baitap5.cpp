276bd15dff6f7ade7fe25038ec0ac05985470724
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-10 06:39:41 -0800

    Add files via upload

---
+++ baitap5.cpp
@@ -0,0 +1,9 @@
+#include<stido.h>
+int main(){
+    int chieuRong =3, chieuDai=5 ,chuVi,dienTich;
+    chuVi =(chieuRong+chieuDai)/2;
+    printf("chu vi hinh chu nhat la %d\n",chuVi);
+    dienTich =chieuRong*chieuDai;
+    printf("dien tich hinh chu nhat la %d\n",dienTich;
+    return 0;
+}
