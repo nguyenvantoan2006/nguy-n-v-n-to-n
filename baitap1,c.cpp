8167c704a089373d86b1314c473f7e3b89f1b591
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-10 06:38:22 -0800

    Add files via upload

---
+++ baitap1,c.cpp
@@ -0,0 +1,15 @@
+#include <studio.h>
+//int main(){
+int age=18
+printf("Toi %d tuoi \n",age);
+//float la bien de luu tru so thap phan
+float height = 55.5;
+printf("Toi nang %.1f ky\n" , height);
+duoble toaDo = 24.4444 ;
+printf("Toa do nha toi la % 4f \n", toaDo);
+//char la bien de luu tru ki tu
+char text = 'a';
+printf("ky tu la %c \n", text);
+
+  return 0;
+}       
