cfb7ac937fbc7a1d623936478c108997691ebd43
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-10 06:39:28 -0800

    Add files via upload

---
+++ baitap4.cpp
@@ -0,0 +1,9 @@
+#include<stido.h> 
+int main(){
+    int canh_hinh_vuong=5, chu_vi,dien_tich;
+    chu_vi= canh hinh vuong*4;
+    printf("Chu vi hinh vuong la %d\n",chu_vi);
+    dien_tich = canh_hinh_vuong*canh_hinh_vuong;
+    printf("Dien tich hinh vuong la %d\n",dien_tich)
+    return 0;
+}
