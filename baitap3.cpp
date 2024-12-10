f01d9736142a2529d0f1a8f081b5809af20d98ea
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-10 06:39:15 -0800

    Add files via upload

---
+++ baitap3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=10 ,b=30 ,tong,hieu,tich,thuong;
+    tong=a+b;
+    printf("tong so nguyen cua ban la %d\n",tong);
+    hieu=a-b;
+    printf("hieu so nguyen cua ban la %d\n",hieu);
+    tich=a*b;
+    printf("tich so nguyen cua ban la %d\n",tich);
+    thuong=a/b
+    printf("thuong so nguyen cua ban la %d\n",thuong);
+    return 0;
+}   
+     
