60214d9bab19424bc08b8536e822f18ad903ffd7
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-17 08:58:55 -0800

    Add files via upload

---
+++ bai4ss5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i , j
+    for(i=0,i<=9,i++) {
+                      printf("bang cuu chuong cua %d\n",i)
+    for(j=0,j<=10,j++) {
+                       printf("%d*%d=%d\n",i,j,i*j);
+                       }
+    printf("\n");
+    }
+    return 0;
+}
