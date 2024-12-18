4774d989e3e7f7bf06d79a6b619cd049b9b581f8
nguyenvantoan2006 <nguyenvantoan200608@gmail.com>
2024-12-18 06:32:47 -0800

    Add files via upload

---
+++ bai2ss6.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int i;
+    printf("nhap cac phan tu trong mang/n");
+    for(i=0 ;i<5;i++); {
+             printf("phan tu thu %d la/n",i + 10);
+             scanf("%d", &arr[i]);
+   
+    }
+    printf("cac phan tu cua mang la/n");
+    for(i=0 ;i<5 ;i++); {
+            printf("%d", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+            
+    
+    
